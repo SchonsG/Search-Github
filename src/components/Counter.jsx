c18228895ac639d663a22837d7321a0e585c8933
SchonsG <gschonswow@gmail.com>
2021-03-17 23:21:53 -0300

    Conceitos importantes

---
+++ src/components/Counter.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react';
+
+export function Counter() {
+
+  let [counter, setCounter] = useState(0);
+
+  function increment() {
+    setCounter(counter ++);
+  }
+
+  return (
+
+    <div>
+      <h2>Count: {counter}</h2>
+      <button type='button' onClick={increment}>
+        Increment
+      </button>
+    </div>
+
+  );
+}
